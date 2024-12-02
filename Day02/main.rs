041d308067c28a435fe9ed84a43b000ac3ecc34a
ProfessionalMenace <vondracekadam00@gmail.com>
2024-12-02 23:13:42 +0100

    Update main.rs

--- Day02/main.rs
+++ Day02/main.rs
@@ -1,6 +1,15 @@
 use std::fs;
 use std::time::Instant;
 
+fn check_safety(line: &Vec<i32>) -> bool {
+    let safety1 = line.is_sorted_by(|a,b| a < b);
+    let safety2 = line.is_sorted_by(|a,b| a > b);
+    let safety3 = line.windows(2)
+        .map( | window | (window[0] - window[1]))
+        .all( | diff | diff.abs() < 4 && diff.abs() > 0);
+    return safety3 && (safety1 || safety2);
+}
+
 fn main() {
     let time_start = Instant::now();
     let text: String = fs::read_to_string("input").unwrap();
@@ -15,54 +24,38 @@ fn main() {
         })
         .collect::<Vec<Vec<i32>>>();
 
-    let mut mask: Vec<bool> = vec![true; parsed_lines.len()];
-
-    for (i, line) in parsed_lines.iter().enumerate() {
-        let mut prev: i32 = line[0];
-        let mut pdiff:i32 = line[0] - line[1];
-
-        for &num in &line[1..] {
-            let diff = prev - num;
-            if diff.abs() > 3 || diff.abs() == 0 || diff.signum() != pdiff.signum() {
-                mask[i] = false;
-                break;
+    let ans1 = parsed_lines
+        .iter()
+        .fold(0, |acc, line| {
+            if check_safety(&line) { 
+                acc + 1
+            } else {
+                acc
             }
-            pdiff = diff;
-            prev = num;
-        }
-    }
-    let ans1 = mask.iter().filter(|&&val| val == true).count();
+        });
 
-    for (i, line) in parsed_lines.iter().enumerate() {
-        if mask[i] == true { continue; }
-        for j in 0..line.len() {
-            let mut nline: Vec<i32> = Vec::new();
-            for (k, num) in line.iter().enumerate() {
-                if j != k {
-                    nline.push(*num);
+    let ans2 = parsed_lines
+        .iter()
+        .fold(0, |acc, line| {
+            let mut safety = false;
+            for i in 0..line.len() {
+                let mut vec = Vec::new();
+                for (j, &num) in line.iter().enumerate() {
+                    if i != j { vec.push(num); }
                 }
-            }
 
-            let mut safety: bool = true;
-            let mut prev:  i32 = nline[0];
-            let mut pdiff: i32 = nline[0] - nline[1];
-
-            for &num in &nline[1..] {
-                let diff = prev - num;
-                if diff.abs() > 3 || diff.abs() == 0 || diff.signum() != pdiff.signum() {
-                    safety = false;
+                safety = safety || check_safety(&vec);
+                if safety { 
                     break;
                 }
-                pdiff = diff;
-                prev = num;
             }
-
             if safety {
-                mask[i] = true;
+                acc + 1
+            } else {
+                acc
             }
-        }
-    }
+        });
 
-    let ans2 = mask.iter().filter(|&&val| val == true).count();
-    println!("{} {} {:.5?}", ans1, ans2, time_start.elapsed());
+
+        println!("{} {} {:.2?}", ans1, ans2, time_start.elapsed());
 }
