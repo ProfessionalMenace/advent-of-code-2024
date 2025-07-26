011a4a195c0a8cf59fde852f50378ca59658b3c7
ProfessionalMenace <vondracekadam00@gmail.com>
2025-07-26 22:51:24 +0200

    day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 macro_rules! solve {
     ($day:ident) => {
@@ -12,4 +13,5 @@ fn main() {
     solve!(day1);
     solve!(day2);
     solve!(day3);
+    solve!(day4);
 }
