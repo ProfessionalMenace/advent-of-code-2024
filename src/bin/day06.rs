67edb0f110a4cb2bfadf2d549413d4a76bee6b7c
ProfessionalMenace <vondracekadam00@gmail.com>
2025-08-24 18:11:29 +0200

    Minor improvements

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -9,22 +9,6 @@ fn parse(path: &str) -> Vec<Vec<u8>> {
         .collect()
 }
 
-fn find_start_pos(mat: &Vec<Vec<u8>>) -> Option<(usize, usize)> {
-    for i in 0..mat.len() {
-        for j in 0..mat[i].len() {
-            if mat[i][j] == b'^' {
-                return Some((i, j));
-            };
-        }
-    }
-    return None;
-}
-
-fn check_bounds(pos: (usize, usize), mat: &Vec<Vec<u8>>) -> bool {
-    let (row, col) = pos;
-    return (row > 0) && (row + 1 < mat.len()) && (col > 0) && (col + 1 < mat[row].len());
-}
-
 #[derive(Eq, Hash, PartialEq, Clone, Copy)]
 enum Direction {
     North,
@@ -33,65 +17,93 @@ enum Direction {
     West,
 }
 
-fn step(pos: &(usize, usize), dir: &Direction) -> (usize, usize) {
-    let (x, y) = *pos; 
-    return match dir {
-        Direction::North => (x - 1, y),
-        Direction::East => (x, y + 1),
-        Direction::South => (x + 1, y),
-        Direction::West => (x, y - 1),
-    };
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
+struct GridWalker {
+    pub row: usize,
+    pub col: usize,
+    pub dir: Direction,
 }
 
-fn turn_right(dir: &Direction) -> Direction {
-    return match dir {
-        Direction::North => Direction::East,
-        Direction::East => Direction::South,
-        Direction::South => Direction::West,
-        Direction::West => Direction::North,
-    };
-}
+impl GridWalker {
+    fn turn_right(&mut self) {
+        self.dir = match self.dir {
+            Direction::North => Direction::East,
+            Direction::East => Direction::South,
+            Direction::South => Direction::West,
+            Direction::West => Direction::North,
+        };
+    }
 
-fn walk(pos: &mut (usize, usize), dir: &mut Direction, mat: &Vec<Vec<u8>>) {
-    let next_pos = step(pos, dir);
-    if mat[next_pos.0][next_pos.1] == b'#' {
-        *dir = turn_right(dir);
-    } else {
-        *pos = next_pos; 
+    fn step_forward(&mut self) {
+        match self.dir {
+            Direction::North => self.row -= 1,
+            Direction::East  => self.col += 1,
+            Direction::South => self.row += 1,
+            Direction::West  => self.col -= 1,
+        }
     }
 }
 
-fn grid_walker(mat: &Vec<Vec<u8>>, mut pos: (usize, usize), mut dir: Direction) -> HashSet<(usize, usize)> {
+fn find_start_pos(mat: &Vec<Vec<u8>>) -> Option<GridWalker> {
+    for i in 0..mat.len() {
+        for j in 0..mat[i].len() {
+            if mat[i][j] == b'^' {
+                return Some(GridWalker{row: i, col: j, dir: Direction::North});
+            };
+        }
+    }
+    return None;
+}
+
+fn check_bounds(mat: &Vec<Vec<u8>>, walker: &GridWalker) -> bool {
+    return (walker.row > 0) 
+        && (walker.row + 1 < mat.len()) 
+        && (walker.col > 0) 
+        && (walker.col + 1 < mat[walker.row].len());
+}
+
+fn walk(mat: &Vec<Vec<u8>>, walker: &mut GridWalker) {
+    let mut next_pos = walker.clone();
+    next_pos.step_forward();
+    if mat[next_pos.row][next_pos.col] == b'#' {
+        walker.turn_right();
+    } else {
+        *walker = next_pos; 
+    }
+}
+
+fn unique_pos(mat: &Vec<Vec<u8>>, mut walker: GridWalker) -> HashSet<(usize, usize)> {
     let mut walked_path: HashSet<(usize, usize)> = HashSet::new();
-    while check_bounds(pos, &mat) {
-        walk(&mut pos, &mut dir, &mat);
-        walked_path.insert(pos);
+    walked_path.insert((walker.row, walker.col));
+    while check_bounds(&mat, &walker) {
+        walk(&mat, &mut walker);
+        walked_path.insert((walker.row, walker.col));
     }
     return walked_path;
 }
 
-fn loopy_walker(mat: &Vec<Vec<u8>>, mut pos: (usize, usize), mut dir: Direction) -> bool {
-    let mut walked_path: HashSet<((usize, usize), Direction)> = HashSet::new();
-    while check_bounds(pos, &mat) {
-        if walked_path.contains(&(pos, dir)) {
+fn detect_loop(mat: &Vec<Vec<u8>>, mut walker: GridWalker) -> bool {
+    let mut walked_path: HashSet<GridWalker> = HashSet::new();
+    while check_bounds(&mat, &walker) {
+        if walked_path.contains(&walker) {
             return true;
         }
-        walked_path.insert((pos, dir));
-        walk(&mut pos, &mut dir, &mat);
+        walked_path.insert(walker);
+        walk(&mat, &mut walker);
     }
     return false;
 }
 
-fn solve_part_one(mat: Vec<Vec<u8>>, pos: (usize, usize), dir: Direction) -> usize {
-   grid_walker(&mat, pos, dir).len() 
+fn solve_part_one(mat: Vec<Vec<u8>>, walker: GridWalker) -> usize {
+   unique_pos(&mat, walker).len()
 }
 
-fn solve_part_two(mut mat: Vec<Vec<u8>>, pos: (usize, usize), dir: Direction) -> usize {
+fn solve_part_two(mut mat: Vec<Vec<u8>>, walker: GridWalker) -> usize {
     let mut ans: usize = 0;
-    for (x, y) in grid_walker(&mat, pos, dir) {
+    for (x, y) in unique_pos(&mat, walker) {
         let tmp = mat[x][y];
         mat[x][y] = b'#';
-        if loopy_walker(&mat, pos, dir) {
+        if detect_loop(&mat, walker) {
             ans += 1;
         }
         mat[x][y] = tmp;
@@ -108,7 +120,7 @@ mod tests {
         let grid = parse("inputs/day06.txt");
         let start_pos = find_start_pos(&grid).unwrap();
 
-        let part_one = solve_part_one(grid.clone(), start_pos, Direction::North);
+        let part_one = solve_part_one(grid.clone(), start_pos);
         assert_eq!(part_one, 5030);
     }
 
@@ -117,7 +129,7 @@ mod tests {
         let grid = parse("inputs/day06.txt");
         let start_pos = find_start_pos(&grid).unwrap();
 
-        let part_two = solve_part_two(grid.clone(), start_pos, Direction::North);
+        let part_two = solve_part_two(grid.clone(), start_pos);
         assert_eq!(part_two, 1928);
     }
 }
